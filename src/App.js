5cfb71a7b7b0bbacca487c5e7eecf20750fd503d
Solomatin Mikhail <mi.solomatin@ase-ec.ru>
2022-05-30 12:42:19 +0300

    remove tree root

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import _ from 'lodash';
 import { useEffect, useState } from 'react';
 import TreeMenu from 'react-simple-tree-menu';
 import '../node_modules/react-simple-tree-menu/dist/main.css';
@@ -6,7 +7,8 @@ import './App.css';
 
 function App() {
   const [item, setItem] = useState('');
-  const [res, setContent] = useState(null);
+  const [content, setContent] = useState(null);
+  const [tree, setTree] = useState('');
 
   useEffect(() => {
     async function getResults() {
@@ -15,6 +17,27 @@ function App() {
     }
     getResults();
   }, []);
+
+  useEffect(() => {
+    let data = [];
+    if (content) {
+      data = JSON.parse(content.files['view.json'].content).entityLabelPages[0];
+
+      const node = [];
+
+      for (let i = 0; i < data.labels.length; i++) {
+        node.push({
+          key: data.entityLongIds[i],
+          label: data.labels[i],
+          parent: data.parentEntityLongIds[i] === -1 ? null : data.parentEntityLongIds[i],
+        });
+      }
+
+      setTree(buildTree(node));
+    }
+  },
+    [content]
+  )
 
   function buildTree(node) {
     const map = new Map(node.map(item => [item.key, item]));
@@ -28,25 +51,6 @@ function App() {
     return [...map.values()].filter(item => !item.parent);
   }
 
-  let data = [];
-  let tree = [];
-
-  if (res) {
-    data = res && JSON.parse(res.files['view.json'].content).entityLabelPages[0];
-
-    const node = [];
-
-    for (let i = 0; i < data.labels.length; i++) {
-      node.push({
-        key: data.entityLongIds[i],
-        label: data.labels[i],
-        parent: data.parentEntityLongIds[i] === -1 ? null : data.parentEntityLongIds[i],
-      });
-    }
-
-    tree = buildTree(node);
-  }
-
   const handleApplyClick = () => {
     console.log('####: ~ file: App.js ~ line 22 ~ App ~ tree', tree);
   }
@@ -55,6 +59,37 @@ function App() {
     document.location.reload();
   }
 
+  const handleRemoveClick = () => {
+    console.log('item', item);
+    let filteredTree = _.cloneDeep(tree);
+    console.log('####: ~ file: App.js ~ line 64 ~ handleRemoveClick ~ filteredTree', filteredTree);
+
+    const keys = item.key.split('/').map(i => parseInt(i));
+
+    const keysWithoutFirst = keys.splice(1);
+
+    let parentNode = null;
+    let indexNode = null;
+    let currentNode = filteredTree.find((node, index) => {
+      indexNode = index;
+      return node.key === keys[0];
+    });
+
+    for (const k of keysWithoutFirst) {
+      parentNode = currentNode;
+      currentNode = currentNode.nodes.find(node => node.key === k)
+    }
+
+    const nodeToDelete = currentNode;
+
+    if (parentNode == null) {
+      filteredTree = filteredTree.filter(node => node.key !== nodeToDelete.key);
+    } else {
+      parentNode = parentNode.nodes.filter(node => (node.key !== nodeToDelete.key));
+    }
+
+    setTree(filteredTree);
+  }
 
   return (
     <div className="root">
@@ -70,20 +105,19 @@ function App() {
         />
       </div>
       <div className="wrapper">
+        <div className="button__wrapper">
+          <button onClick={handleRefreshClick}>Refresh</button>
+          <button onClick={handleApplyClick}>Apply</button>
+          <button onClick={handleRemoveClick}>Remove</button>
+        </div>
         {item && <div className="description">
           <p>Label: {item.label}</p>
           <p>Id: {item.key.split('/').pop()}</p>
           <p>ParentId: {item.parent === null ? '-1' : item.parent}</p>
         </div>}
-        <div className="button__wrapper">
-          <button onClick={handleRefreshClick}>Refresh</button>
-          <button onClick={handleApplyClick}>Apply</button>
-        </div>
       </div>
     </div>
   );
 }
 
 export default App;
-
-
